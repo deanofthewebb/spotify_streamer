9a0a7b9fe3b18698edb8c5f0cdd3c13390992fb2
Dean Webb <deanmwebb@gmail.com>
2015-07-03 23:49:03 -0400

    Adding Artist Track Detail Layout

---
+++ app/src/main/java/app/com/deanofthewebb/spotifystreamer/ArtistDetailActivityFragment.java
@@ -0,0 +1,20 @@
+package app.com.deanofthewebb.spotifystreamer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ArtistDetailActivityFragment extends Fragment {
+
+    public ArtistDetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_artist_detail, container, false);
+    }
+}
